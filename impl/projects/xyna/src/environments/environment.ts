6554ab56a14bb47fc9b9aa9733127038b8b1567a
Johannes Heucher <johannes.heucher@gip.com>
2023-10-20 12:55:11 +0200

    updated modules for material v15

--- impl/projects/xyna/src/environments/environment.ts
+++ impl/projects/xyna/src/environments/environment.ts
@@ -27,7 +27,7 @@ import { RuntimeContext } from '@zeta/api/xo/xo-describer';
 export const environment: ZetaEnvironment = {
     production: false,
     zeta: {
-        url: 'https://host-ip-address:host-port/modeller-api/',
+        url: 'https://10.2.8.155/modeller-api/',
         xo: {
             runtimeContext: RuntimeContext.fromApplication('GuiHttp'),
             consistencyCheck: false
